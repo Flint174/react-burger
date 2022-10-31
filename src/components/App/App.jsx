6324dc2033d4542ee5952c0009e66bab372c6851
Egor Koksharov <yegor.koksharov@gmail.com>
2022-10-31 15:47:08 +0300

    fetch request

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,18 @@
 import { AppHeader } from "../AppHeader";
 import { AppMain } from "../AppMain";
-import { data } from "../../utils/data";
+import { useEffect, useState } from "react";
+import { ingredientsUrl } from "../../utils/constants";
 
 export const App = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch(ingredientsUrl)
+            .then(res => res.json())
+            .then(json => setData(json.data))
+            .catch(err => console.error(err))
+    }, [])
+
     return (
         <>
             <AppHeader />
