5a5c4f643d942ac374867eb97982247b38925af2
Egor <e.koksharov@incotex.ru>
2022-11-17 16:23:44 +0300

    remove ingredient from the constructor

--- src/components/burger-constructor/composition-list.jsx
+++ src/components/burger-constructor/composition-list.jsx
@@ -3,31 +3,39 @@ import style from "./style.module.css";
 import { CompositionListItem } from "./composition-list-item";
 import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
 import { ConstructorElementEmpty } from "./constructor-element-empty";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { removeIngredient } from "../../services/slices/constructor-slice";
+import { useMemo } from "react";
 
 export const CompositionList = () => {
     const ingredients = useSelector(store => store.constructorReducer.ingredients)
+    const dispatch = useDispatch()
+
+    const compositionList = useMemo(() =>
+        Array.isArray(ingredients) && ingredients.length
+            ? ingredients.map(el => (
+                <CompositionListItem key={el.uuid}>
+                    <ConstructorElement
+                        text={el.name}
+                        thumbnail={el.image}
+                        price={el.price}
+                        handleClose={() => dispatch(removeIngredient(el.uuid))}
+                    />
+
+                </CompositionListItem>
+            )) : (
+                <CompositionListItem isLocked={true}>
+                    <ConstructorElementEmpty
+                        text={'Выберите ингредиент'}
+                    />
+                </CompositionListItem>
+            )
+        , [ingredients])
+
+
     return (
         <div className={clsx(style.ingredients_scroll, "flex column gap-4")} >
-            {
-                Array.isArray(ingredients) && ingredients.length
-                    ? ingredients.map(el => (
-                        <CompositionListItem key={el.uuid}>
-                            <ConstructorElement
-                                text={el.name}
-                                thumbnail={el.image}
-                                price={el.price}
-                            />
-
-                        </CompositionListItem>
-                    )) : (
-                        <CompositionListItem isLocked={true}>
-                            <ConstructorElementEmpty
-                                text={'Выберите ингредиент'}
-                            />
-                        </CompositionListItem>
-                    )
-            }
+            {compositionList}
         </div>
     )
 }
